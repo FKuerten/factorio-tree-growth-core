1d25d2398e1f2a0d9c5ad31a6ede966b13f3669e
Fabian "Graveeater" Kürten <graveeater@sitl.de>
2019-12-01 18:03:46 +0100

    support for more complicated definitions
    api compatible

--- persistence/readControl.lua
+++ persistence/readControl.lua
@@ -1,12 +1,31 @@
-function getTreeData(treeName)
-  local textName = 'tree-growth-core-' .. treeName
-  local textPrototype = game.entity_prototypes[textName]  
+local readFlyingText = function(textName)
+  local textPrototype = game.entity_prototypes[textName]
   if not textPrototype then return nil end
   local order = textPrototype.order
-  if not order then return nil end
-  local dataFunc = loadstring(order)
+  return order
+end
+
+local parseData = function(serializedData)
+  local dataFunc = loadstring(serializedData)
   if not dataFunc then return nil end
   --assert(dataFunc, "no data for " .. treeName .. " order=" .. order)
   local data = dataFunc()
+  return data
+end
+
+function getTreeData(treeName)
+  local textName = 'tree-growth-core-' .. treeName
+
+  local serializedData = ""
+  local chunkIndex = 1
+  while true do
+    local chunkName = textName .. "-" .. chunkIndex
+    local serializedDataChunk = readFlyingText(chunkName)
+    if not serializedDataChunk then break end
+    serializedData = serializedData .. serializedDataChunk
+    chunkIndex = chunkIndex + 1
+    --game.print(chunkName .. serializedDataChunk)
+  end
+  local data = parseData(serializedData)
   return data
 end
--- persistence/writeData.lua
+++ persistence/writeData.lua
@@ -1,12 +1,14 @@
 tree_growth = tree_growth or {}
 tree_growth.core = tree_growth.core or {}
 
-tree_growth.core.persist = function(treeName, treeData)
-  assert(treeName, "need a name to persist")
-  local textName = 'tree-growth-core-' .. treeName
+local chunkSize = 200
+
+local serialize = function(treeData)
   local serializedData = serpent.dump(treeData)
-  
-  -- actually store it
+  return serializedData
+end
+
+local storeInFlyingText = function(textName, serializedData)
   if data.raw['flying-text'][textName] then
     data.raw['flying-text'][textName].order = serializedData
   else
@@ -17,5 +19,20 @@ tree_growth.core.persist = function(treeName, treeData)
       speed = 1,
       order = serializedData
     }})
+  end
+end
+
+tree_growth.core.persist = function(treeName, treeData)
+  assert(treeName, "need a name to persist")
+  local textName = 'tree-growth-core-' .. treeName
+  local serializedData = serialize(treeData)
+  
+  local length = serializedData:len()
+  local chunks = math.ceil(length / chunkSize)
+  for chunkIndex = 1, chunks do
+    local chunkName = textName .. "-" .. chunkIndex
+    local chunkStart = (chunkIndex - 1) * chunkSize + 1
+    local serializedDataChunk = serializedData:sub(chunkStart, chunkStart + chunkSize - 1)
+    storeInFlyingText(chunkName, serializedDataChunk)
   end
 end
